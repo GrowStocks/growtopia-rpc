31776ec7e0edafdcee4864f28caebdb0c70b2c5b
MrAugu <mraugu@yahoo.com>
2021-05-16 01:54:23 +0300

    fix(main file): Smoothed out the process warnings

--- src/index.js
+++ src/index.js
@@ -6,17 +6,5 @@ const manager = new Manager({
 });
 
 process.title = `Growtopia Discord Rich Presence by GrowStocks (${manager.updater.version})`;
-
-/*
- * process.on("unhandledRejection", err => {
- *  manager.emit("log", "[Manager]: Process thrown an unhandled rejection.");
- *  manager.emit("debug", `[Manager]: Unhandled rejection: ${err}`);
- *});
- *
- *process.on("warning", err => {
- *manager.emit("log", "[Manager]: Process warning encountered.");
- *manager.emit("debug", `[Manager]: Process warning: ${err}`);
- *});
- */
-
-console.log(`[Manager]: Starting with client id ${manager.clientId}.`); // eslint-disable-line no-console
+process.on("unhandledRejection", manager.handleError);
+process.on("warning", manager.handleError);
